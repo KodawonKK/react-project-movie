a57ea56c7bd1094a465b220cfc3d074d7cd2def7
KodawonKK <rhekdnjs0169@gmail.com>
2024-09-11 23:34:15 +0900

    useEffect 의존성배열

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("call the api");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("keyword");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("counter");
+  }, [counter]);
+  useEffect(() => {
+    console.log("keword&counter");
+  }, [keyword, counter]);
   return (
     <div>
+      <input type="text" placeholder="Search here" value={keyword} onChange={onChange} />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
