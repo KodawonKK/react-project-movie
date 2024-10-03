d1d46029254e42d3828b7da9f9784d32645bd2c0
KodawonKK <rhekdnjs0169@gmail.com>
2024-10-03 22:54:56 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Movie from "./components/Movie";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
